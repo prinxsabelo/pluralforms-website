76080314f634f8aa127e389ceec2b7a72c85a28a
Bello Samuel <prinxsabelo@gmail.com>
2022-02-08 18:14:12 +0100

    ..

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,9 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script id='google-script' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-      <Script strategy="lazyOnload">
+      <Script id='google-scrip1' strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
