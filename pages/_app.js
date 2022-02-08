b389ebb35f910635fa47606313069333e25cb75f
Bello Samuel <prinxsabelo@gmail.com>
2022-02-08 18:11:48 +0100

    ..

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
       <Head>
         <title>PluralForms - Africa Online Form Builder </title>
         <meta charSet='utf-8' />
