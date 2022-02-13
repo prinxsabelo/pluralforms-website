b98250544479b33288fde88e07fe2840d860263e
Bello Samuel <prinxsabelo@gmail.com>
2022-02-13 01:20:25 +0100

    Robots removed

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,6 @@ function MyApp({ Component, pageProps }) {
                                          create forms,  plural forms, form analysis, online form builder"/>
         <meta name="description" content="Completely blended for you to create forms 
                                           and get detailed analysis. No login, No hassle. PluralForms is free"/>
-        <meta name="robots" content="noindex,follow" />
 
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="628" />
